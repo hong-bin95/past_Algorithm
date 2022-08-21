895aeb788e0c5ae55fbdcb0edcc74719498d5581
parkhongbin95 <parkhongbin95@gmail.com>
2022-08-21 19:31:05 +0900

    0821

--- src/homework/Main_bj_15683_감시_서울_20반_박홍빈.java
+++ src/homework/Main_bj_15683_감시_서울_20반_박홍빈.java
@@ -172,9 +172,33 @@ class CCTV {
 	int x;
 	int y;
 
-	CCTV(int num, int x, int y) {
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
 		this.num = num;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
 		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
 		this.y = y;
+	}
+
+	CCTV(int num, int x, int y) {
+		setNum(num);
+		setX(x);
+		setY(y);
 	}
 }
